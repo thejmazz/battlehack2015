9d5840d8b9468eb77cca3bd1cdabb011add9eae9
ACalza <albert.calzaretto@gmail.com>
2015-07-18 15:49:24 -0400

    More changes

--- api.js
+++ api.js
@@ -3,7 +3,59 @@ var express = require('express');
 var colors = require('colors');
 var mongoose = require('mongoose');
 
+var client = require('twilio')('AC44e4193dd5a5565845962f8f0cd23657', 'a5660fce732c184fad44538b12e89083');
 
+/*
+client.messages.list(function(err, data) {
+    var msg = data.messages.pop();
+
+    client.messages(msg.sid).get(function(err, message) {
+      console.log(message.body);
+  });
+
+});
+/*clientSID.forEach(function(err, data){
+  client.sendMessage({
+      to:'+16474634380', // Any number Twilio can deliver to
+      from: '+12892160973', // A number you bought from Twilio and can use for outbound communication
+      body: 'replying to your message' // body of the SMS message
+
+  }, function(err, responseData) { //this function is executed when a response is received from Twilio
+      if (!err) { // "err" is an error received during the request, if any
+
+          // "responseData" is a JavaScript object containing data received from Twilio.
+          // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+          // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+          // outputs "+14506667788"
+          console.log(responseData.sid);
+          clientSID.push(responseData.sid)// outputs "word to your mother."
+      }
+      else{
+        console.log(err);
+      }
+  });
+
+})
+/*client.sendMessage({
+    to:'+16474634380', // Any number Twilio can deliver to
+    from: '+12892160973', // A number you bought from Twilio and can use for outbound communication
+    body: 'word to your mother.' // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        // outputs "+14506667788"
+        clientSID.push(responseData.sid)// outputs "word to your mother."
+    }
+    else{
+      console.log(err);
+    }
+});*/
 
 // ==== App ====
 var App = global.App = require('./lib/App');
@@ -11,6 +63,7 @@ var port = App.config().port;
 
 // ==== Express ====
 var app = express();
+
 
 // ==== Connect to MongoDB ====
 //mongoose.connect('mongodb://localhost/fba')
---
+++ routes/smsreq/index.js
@@ -0,0 +1,24 @@
+var router = require('express').Router();
+var client = require('twilio')('AC44e4193dd5a5565845962f8f0cd23657', 'a5660fce732c184fad44538b12e89083');
+var twilio = require('twilio')
+router.get('/', function(req, res) {
+  var resp = new twilio.TwimlResponse();
+
+ // The TwiML response object will have functions on it that correspond
+ // to TwiML "verbs" and "nouns". This example uses the "Say" verb.
+ // Passing in a string argument sets the content of the XML tag.
+ // Passing in an object literal sets attributes on the XML tag.
+ resp.message('ahoy hoy! Testing Twilio and node.js');
+
+ //Render the TwiML document using "toString"
+ res.writeHead(200, {
+     'Content-Type':'text/xml'
+ });
+ res.end(resp.toString());
+
+});
+router.get('/messages', function(req, res){
+
+})
+
+module.exports = router;
