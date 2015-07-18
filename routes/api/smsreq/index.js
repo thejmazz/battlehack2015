050baf94a2411fc97f2c92b46f6819fae663180b
ACalza <albert.calzaretto@gmail.com>
2015-07-18 18:27:57 -0400

    test

--- routes/api/smsreq/index.js
+++ routes/api/smsreq/index.js
@@ -1,20 +1,23 @@
 var router = require('express').Router();
 var client = require('twilio')('AC44e4193dd5a5565845962f8f0cd23657', 'a5660fce732c184fad44538b12e89083');
-var twilio = require('twilio')
+var twilio = require('twilio');
+var feed = require("feed-read");
 router.get('/', function(req, res) {
   var resp = new twilio.TwimlResponse();
   var text = '';
- // The TwiML response object will have functions on it that correspond
- // to TwiML "verbs" and "nouns". This example uses the "Say" verb.
- // Passing in a string argument sets the content of the XML tag.
- // Passing in an object literal sets attributes on the XML tag.
+  // The TwiML response object will have functions on it that correspond
+  // to TwiML "verbs" and "nouns". This example uses the "Say" verb.
+  // Passing in a string argument sets the content of the XML tag.
+  // Passing in an object literal sets attributes on the XML tag.
+
 
  //resp.message('ahoy hoy! Testing Twilio and node.js');
- console.log(resp)
+
  switch(req.query.Body.toLowerCase()){
 
    case "commands":
       resp.message("you requested help");
+
       res.writeHead(200, {
           'Content-Type':'text/xml'
       });
@@ -29,24 +32,21 @@ router.get('/', function(req, res) {
       });
 
       res.end(resp.toString());
-
       break;
-   default: test(req.query.Body, function(err, txt){
-     text = txt;
-     resp.message("Please wait a moment");
-     resp.message(txt);
-     //Render the TwiML document using "toString"
-     res.writeHead(200, {
-         'Content-Type':'text/xml'
-     });
-
-     res.end(resp.toString());
-
-  });
+  default: test(req.query.Body, function(err, txt){
+      text = txt;
+      resp.message("Please wait a moment");
+      resp.message(txt);
+      //Render the TwiML document using "toString"
+      res.writeHead(200, {
+        'Content-Type':'text/xml'
+      });
+      res.end(resp.toString());
+    });
   }
 });
 
-var feed = require("feed-read");
+
 function test(rss, callback){
   feed(rss, function(err, articles) {
     if (err) callback(err)
