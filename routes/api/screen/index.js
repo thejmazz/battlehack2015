15c74d8e81e4624e2ece95aa16fb93633fff3b38
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-07-18 16:37:20 -0400

    basic phantomjs screen grab

--- routes/api/screen/index.js
+++ routes/api/screen/index.js
@@ -1,7 +1,24 @@
 var router = require('express').Router();
+var path = require('path');
+var cp = require('child_process');
+var phantomjs = require('phantomjs');
+var binPath = phantomjs.path;
 
 router.get('/', function(req, res) {
-    res.send('working in screen\n');
+
+    console.log(binPath);
+
+    var childArgs = [
+      path.join(__dirname, '../../../phantom', 'github.js')
+    ]
+
+    cp.execFile(binPath, childArgs, function(err, stdout, stderr) {
+        console.log('err ' + err);
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+
+        res.send('finished?\n');
+    });
 });
 
 module.exports = router;
