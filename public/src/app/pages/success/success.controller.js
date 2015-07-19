b0933f15922a8ea3b62044caced40b50d595c5f7
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-07-19 09:15:28 -0400

    ssssssssssssssssssssssssssssss

--- public/src/app/pages/success/success.controller.js
+++ public/src/app/pages/success/success.controller.js
@@ -7,19 +7,17 @@ angular.module('smser')
 .controller('SuccessCtrl', ['$scope', '$location', function($scope, $location) {
 
     var paymentId = $location.search().paymentId;
-    console.log(paymentId);
+    //console.log(paymentId);
     var token = $location.search().token;
-    console.log(token);
+    //console.log(token);
     var PayerID = $location.search().PayerID;
-    console.log(PayerID);
+    //console.log(PayerID);
 
-
-    $.get(baseUrl + 'api/purchase/success?paymentId=' + paymentId + '&token=' + token + '&PayerID=' + PayerID).done(function(data){
-      console.log(data);
-    })
 
     $scope.success = function() {
-      alert('sdsdsd');
+      $.get(baseUrl + 'api/purchase/success?paymentId=' + paymentId + '&token=' + token + '&PayerID=' + PayerID).done(function(data){
+        console.log(data);
+      })
     }
 
 }]);
