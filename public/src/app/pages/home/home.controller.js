35c2126e69b4acb61e9b9bcb350b499c3345dce4
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-07-19 06:29:20 -0400

    bunch of css shit for the phone

--- public/src/app/pages/home/home.controller.js
+++ public/src/app/pages/home/home.controller.js
@@ -4,4 +4,29 @@ angular.module('smser')
 
 .controller('HomeCtrl', ['$scope', function($scope) {
     $scope.title = 'SMS';
+
+    $scope.msgs = [{
+        type: 'from',
+        content: 'Welcome to smster!'
+      }, {
+        type: 'to',
+        content: 'What is smster?? #swag'
+    }, {
+      type: 'from',
+      content: 'smster is an sms based API for accessing online content #yolo'
+    }]
+
+    $scope.addMsg = function() {
+      if ($scope.typespace.length > 0) {
+        $scope.msgs.push({
+          type: 'to',
+          content: $scope.typespace
+        });
+        $scope.msgs.push({
+          type: 'from',
+          content: 'API response'
+        })
+        $scope.typespace = '';
+      }
+    };
 }]);
